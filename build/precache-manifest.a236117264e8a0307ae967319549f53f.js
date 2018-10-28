9fcad46007f9a9f569452cf1f66c995a636f0cba
gopesh301 <gopeshsinghal301@gmail.com>
2018-10-29 00:38:43 +0530

    portfolio

---
+++ build/precache-manifest.a236117264e8a0307ae967319549f53f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a4cf4853c511fba1f5b33b38e31f7cd",
+    "url": "/static/media/portfolio-background.6a4cf485.jpg"
+  },
+  {
+    "revision": "4223419a8181259641fbd87e32be4e0f",
+    "url": "/static/media/note.4223419a.png"
+  },
+  {
+    "revision": "e516e5e0f613b59ab28e5bcf3de0e233",
+    "url": "/static/media/my.e516e5e0.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/127 burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dbcf0c92420ffc4ce48f",
+    "url": "/static/js/main.dbcf0c92.chunk.js"
+  },
+  {
+    "revision": "7249b8f405586854e513",
+    "url": "/static/js/1.7249b8f4.chunk.js"
+  },
+  {
+    "revision": "dbcf0c92420ffc4ce48f",
+    "url": "/static/css/main.a4624d65.chunk.css"
+  },
+  {
+    "revision": "19654ae9a8c84a91238cf4d20a4895f5",
+    "url": "/index.html"
+  }
+];
